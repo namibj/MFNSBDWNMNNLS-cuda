1ff477ccff3a8ce8089991289a15b242451a2fef
namibj <namibj@users.noreply.github.com>
2015-08-21 07:20:23 +0200

    Update MFNSBDWNMNNLS.cu

--- cuda/MFNSBDWNMNNLS/src/MFNSBDWNMNNLS.cu
+++ cuda/MFNSBDWNMNNLS/src/MFNSBDWNMNNLS.cu
@@ -1,14 +1,6 @@
 /**
- * Copyright 1993-2012 NVIDIA Corporation.  All rights reserved.
- *
- * Please refer to the NVIDIA end user license agreement (EULA) associated
- * with this source code for terms and conditions that govern your use of
- * this software. Any use, reproduction, disclosure, or distribution of
- * this software and related documentation outside the terms of the EULA
- * is strictly prohibited.
- *
- * modified by Merlin Kramer.
- * to be better annotated.
+ * Copyright 2015 Merlin Kramer
+ * Licensed under the GNU Affero General Public License v3.0
  */
 #include <stdio.h>
 #include <stdlib.h>
